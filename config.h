c01be746a737a8b79a521cce4aa9dbb7b9bc9ab1
Scott Gullaksen <scott.gullaksen@drift.oslo.kommune.no>
2022-05-28 15:49:35 +0200

    Added fullgaps and changed the color scheme

--- config.h
+++ config.h
@@ -1,23 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Ubuntu Nerd Font:style=Medium,Bold:size=13" };
 static const char dmenufont[]       = "Ubuntu Nerd Font:style=Medium,Bold:size=13";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray1[]       = "#222222";  // unselected, background
+static const char col_gray2[]       = "#444444";  // unselected, border
+static const char col_black[]       = "#000000";  // unselected, border
+static const char col_gray3[]       = "#bbbbbb";  // unselected, text
+static const char col_gray4[]       = "#eeeeee";  // selected, text
+static const char col_white[]       = "#ffffff";  // selected, text
 static const char col_cyan[]        = "#005577";
+static const char col_bright_purp[]        = "#7700ff";  // selected, background and border
+static const char col_blue[]        = "#269aff";  // selected, background and border
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_black, col_black },
+	[SchemeSel]  = { col_blue, col_black, col_blue },  // bg here determines both statusbar "selected" colors - how to seperate active tag color
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -36,6 +41,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       "Firefox Preferences",        1 << 8,         True,     -1 },
 };
 
 /* layout(s) */
@@ -92,6 +98,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -120,4 +130,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-

d6458a5658789a28a638a249cda2e10ab5dff9ac
Scott Gullaksen <scott.gullaksen@gmail.com>
2022-11-24 15:23:16 +0100

    Revert no-border patch

--- dwm.c
+++ dwm.c
@@ -221,7 +221,6 @@ static void setup(void);
 static void seturgent(Client *c, int urg);
 static void showhide(Client *c);
 static void sigchld(int unused);
-static int solitary(Client *c);
 static void sigstatusbar(const Arg *arg);
 static void spawn(const Arg *arg);
 static void tag(const Arg *arg);
@@ -967,12 +966,7 @@ focus(Client *c)
 		detachstack(c);
 		attachstack(c);
 		grabbuttons(c, 1);
- 		/* Avoid flickering when another client appears and the border
- 		 * is restored */
- 		if (!solitary(c)) {
- 			XSetWindowBorder(dpy, c->win, scheme[SchemeSel][ColBorder].pixel);
- 		}
- 
+		XSetWindowBorder(dpy, c->win, scheme[SchemeSel][ColBorder].pixel);
 		setfocus(c);
 	} else {
 		XSetInputFocus(dpy, root, RevertToPointerRoot, CurrentTime);
@@ -1477,11 +1471,6 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	c->oldw = c->w; c->w = wc.width = w;
 	c->oldh = c->h; c->h = wc.height = h;
 	wc.border_width = c->bw;
-  if (solitary(c)) {
- 		c->w = wc.width += c->bw * 2;
- 		c->h = wc.height += c->bw * 2;
- 		wc.border_width = 0;
- 	}
 	XConfigureWindow(dpy, c->win, CWX|CWY|CWWidth|CWHeight|CWBorderWidth, &wc);
 	configure(c);
 	XSync(dpy, False);
@@ -1864,15 +1853,6 @@ sigchld(int unused)
 	if (signal(SIGCHLD, sigchld) == SIG_ERR)
 		die("can't install SIGCHLD handler:");
 	while (0 < waitpid(-1, NULL, WNOHANG));
-}
-
-int
-solitary(Client *c)
-{
-	return ((nexttiled(c->mon->clients) == c && !nexttiled(c->next))
-	    || &monocle == c->mon->lt[c->mon->sellt]->arrange)
-	    && !c->isfullscreen && !c->isfloating
-	    && NULL != c->mon->lt[c->mon->sellt]->arrange;
 }
 
 void
